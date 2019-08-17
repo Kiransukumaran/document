08b717415e511156906c79d54d40d2f0899a9fff
Kiran Sukumaran <ks4uofficial@gmail.com>
2019-08-17 13:00:52 +0530

    updated db

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/document/';
+// $config['base_url'] = 'http://localhost/document/';
+$config['base_url'] = 'http://18.224.25.164/document/';
 
 /*
 |--------------------------------------------------------------------------
